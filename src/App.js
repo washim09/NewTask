12dd3599025cd1f758953c5a757751ead92fbfe8
washim09 <you@example.com>
2021-11-14 13:17:11 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Features from './Features'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel" data-interval="false">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="image/1.jpg" className="d-block w-100" alt="" />
+          </div>
+          <div className="carousel-item">
+            <img src="image/2.jpg" className="d-block w-100" alt="" />
+          </div>
+          <div className="carousel-item">
+            <img src="image/3.jpg" className="d-block w-100" alt="" />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      < Features />
     </div>
   );
+  
 }
 
 export default App;
---
+++ src/Features.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+function Features() {
+      return (
+            <>
+
+                  <div className="container mt-5 mb-5">
+                        <div className="row">
+                              <div className="col-lg-3">
+                                    <div className="card" style={{ width: "16rem" }}>
+                                          <div className="card-body col shadow p-3 bg-white">
+                                                <img src="image/icon1.png" alt="" className="card-img-top p-1 myimg mt-4" />
+                                                <h5 className="card-title mt-3 mb-3">Get Inspired</h5>
+                                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ad repellat labore perspiciatis est,</p>
+                                                
+                                          </div>
+                                    </div>
+                              </div>
+                              <div className="col-lg-3">
+                                    <div className="card" style={{ width: "16rem" }}>
+                                          <div className="card-body col shadow p-3 bg-white">
+                                                <img src="image/icon1.png" alt="" className="card-img-top p-1 myimg mt-4" />
+                                                <h5 className="card-title mt-3 mb-3">Give Donation</h5>
+                                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ad repellat labore perspiciatis est,</p>
+                                                
+                                          </div>
+                                    </div>
+                              </div>
+                              <div className="col-lg-3">
+                                    <div className="card" style={{ width: "16rem" }}>
+                                          <div className="card-body col shadow p-3 bg-white">
+                                                <img src="image/icon1.png" alt="" className="card-img-top p-1 myimg mt-4" />
+                                                <h5 className="card-title mt-3 mb-3">Become A Volunteer</h5>
+                                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ad repellat labore perspiciatis est,</p>
+                                                
+                                          </div>
+                                    </div>
+                              </div>
+                              <div className="col-lg-3">
+                                    <div className="card" style={{ width: "16rem" }} >
+                                          <div className="card-body col shadow p-3 bg-white ">
+                                                <img src="image/icon1.png" alt="" className="card-img-top p-1 myimg mt-4" />
+                                                <h5 className="card-title mt-3 mb-3">Help The Children</h5>
+                                                <p className="card-text pl-3 pr-3 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ad repellat labore perspiciatis est,</p>
+                                          </div>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+            </>
+      )
+}
+
+export default Features
